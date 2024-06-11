1a006bea919640cc8cbc3311f540eda10782a1ab
Adam Kwiecien <adam.kwiecien@integration.works>
2024-06-12 11:16:20 +1200

    testing cluster build

--- main.tf
+++ main.tf
@@ -1,136 +1,91 @@
-resource "azurerm_resource_group" "resource_group" {
+data "azurerm_resource_group" "resource_group" {
   name     = "adam-sandbox"
-  location = "Australia East"
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+# resource "azurerm_network_security_group" "public-security-group" {
+#   name                = "public-security-group"
+#   location            = azurerm_resource_group.resource_group.location
+#   resource_group_name = azurerm_resource_group.resource_group.name
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+#   security_rule {
+#     name                       = "AllowWorkHTTPInbound"
+#     priority                   = 200
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "80"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
 
-  cluster_endpoint_public_access = true
+#   tags = {
+#     environment = var.environment
+#   }
+# }
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
+# resource "azurerm_network_security_group" "private-security-group" {
+#   name                = "private-security-group"
+#   resource_group_name = data.azurerm_resource_group.resource_group.name
+#   location            = data.azurerm_resource_group.resource_group.location
 
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnet_ids
+#   tags = {
+#     environment = var.environment
+#   }
+# }
 
-  eks_managed_node_group_defaults = {
-    ami_type       = var.default_ami_type
-    instance_types = var.default_instance_types
-  }
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "vnet"
+#   resource_group_name = data.azurerm_resource_group.resource_group.name
+#   location            = data.azurerm_resource_group.resource_group.location
+#   address_space       = ["10.0.0.0/16"]
+#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  eks_managed_node_groups = {
-    default_node_group = {
-      min_size      = 1
-      max_size      = 10
-      desired_size  = 1
-      disk_size     = 50
-      capacity_type = "SPOT"
+#   subnet {
+#     name           = "subnet1"
+#     address_prefix = "10.0.1.0/24"
+#     security_group = azurerm_network_security_group.private-security-group.id
+#   }
 
-      iam_role_additional_policies = {
-        AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-        CloudWatchAgentServerPolicy        = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-        AmazonSSMManagedInstanceCore       = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      }
+#   # subnet {
+#   #   name           = "subnet2"
+#   #   address_prefix = "10.0.2.0/24"
+#   #   security_group = azurerm_network_security_group.public-security-group.id
+#   # }
 
-      pre_bootstrap_user_data = <<-EOT
-        sudo yum install amazon-ssm-agent -y
-        sudo systemctl enable amazon-ssm-agent
-        sudo systemctl start amazon-ssm-agent
-        sudo yum install amazon-cloudwatch-agent -y
-        sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:AmazonCloudwatch-linux
-      EOT
-    }
-  }
-
-  access_entries = {
-    read_only_access_group = {
-      kubernetes_groups = []
-      principal_arn     = var.cluster_read_only_arn
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-    admin_access_group = {
-      kubernetes_groups = []
-      principal_arn     = var.cluster_admin_arn
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-    deployment_user_access = {
-      kubernetes_groups = []
-      principal_arn     = var.deployment_automation_arn
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-  }
-
-  tags = {
-    Environment = var.environment
-  }
-}
-
+#   tags = {
+#     environment = var.environment
+#   }
+# }
 
 module "aks" {
   source  = "Azure/aks/azurerm"
   version = "~> 8.0.0"
 
-  resource_group_name = azurerm_resource_group.aks.name
-  location            = azurerm_resource_group.aks.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  location            = data.azurerm_resource_group.resource_group.location
 
-  cluster_name    = var.cluster_name
+  prefix    = var.cluster_name
   kubernetes_version = var.cluster_version
 
-  # Resources
   agents_pool_name = "default"
   agents_count = 1
   agents_min_count = 1
   agents_max_count = 10
-  agents_size = "Standard_A2_v2"
+  agents_size = "Standard_D2s_v3"
+
+  role_based_access_control_enabled = true
+  log_analytics_workspace_enabled   = false
+
+  rbac_aad_managed = false
+  rbac_aad  = false
+
+  # vnet_subnet_id  = element(azurerm_virtual_network.vnet.subnet[*].id, count.index)
 
   os_disk_size_gb = 50
   os_disk_type = "Ephemeral"
 
-  vnet_subnet_id = var.vnet_subnet_id
-
   tags = {
     Environment = var.environment
   }
-}
-
-resource "azurerm_resource_group" "aks" {
-  name     = "myAKSResourceGroup"
-  location = "East US"
 }
--- provider.tf
+++ provider.tf
@@ -1,22 +1,22 @@
 # example provider.tf
 terraform {
-  required_version = ">= 1.3"
+  required_version = ">= 1.7"
 
   backend "azurerm" {
-    resource_group_name   = "adam-sandbox"
-    storage_account_name  = "terraform-state-blob"
-    container_name        = "tfstatefiles"
+    resource_group_name  = "backstage-terraform"
+    storage_account_name = "iwterraformstate"
+    container_name       = "tf-state"
+    key                  = "aks-cluster-terraform.tfstate"
   }
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
